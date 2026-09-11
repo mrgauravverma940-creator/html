c04ec60da399e5e081674efae05e570e6a88a5db
mrgauravverma940-creator <mrgauravverma940@gmail.com>
2026-09-11 23:11:57 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+int main()
+ {
+    int a, b;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch (operator) {
+        case '+':
+            printf("%d + %d = %d\n", a, b, a + b);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", a, b, a - b);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", a, b, a * b);
+            break;
+        case '/':
+            if (b != 0) {
+                printf("%d / %d = %.2f\n", a, b, (float)a / b);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+    }
+
+    return 0;
+}
