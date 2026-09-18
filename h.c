d0429b8d66bc0d8c3aaf7391015d176f0aee4bc1
mrgauravverma940-creator <mrgauravverma940@gmail.com>
2026-09-18 21:58:02 +0530

    h.c

---
+++ h.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(){
+     int x = 17;
+     int y = 12;
+     int gaurav = 13;
+    printf("x - %d \n",x);
+    printf("y - %d \n",y);
+    printf("gaurav - %d \n",gaurav);
+    return 0;
+}
